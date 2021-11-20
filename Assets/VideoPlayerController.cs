f5696c4b03add5ce6704ed5bf9267642280cb2f1
Kishou Arima <utkaarshsaha@gmail.com>
2021-11-20 20:32:20 +0530

    First Commit

---
+++ Assets/VideoPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerController : MonoBehaviour
+{
+    public VideoPlayer vp;
+    public Texture defaultTex;
+    public RenderTexture renderTex;
+
+    private void OnEnable()
+    {
+        if (vp != null) vp.Play();
+    }
+    private void OnDisable()
+    {
+        if (vp != null)
+        {
+            vp.Stop();
+            Graphics.Blit(defaultTex, renderTex);
+        }
+    }
+}
